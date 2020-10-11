c634fa8aa4b90ab144887d7524465a1bdf96acb3
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-10-11 20:09:47 +0100

    Add dataclass Rover

---
+++ test/mars_rover_test.py
@@ -0,0 +1,22 @@
+import unittest
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Rover(object):
+    facing: str
+
+    def go(self, instruction):
+        pass
+
+
+class MarsRoverTest(unittest.TestCase):
+    def test_turn_right_N_to_E(self):
+        rover = Rover('N')
+        rover.go('R')
+        self.assertEqual('E', rover.facing)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
